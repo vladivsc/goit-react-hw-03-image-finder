9175babc3171597c4a1f1f6a9c2e0f3a2fd84906
Vladyslav <101827554+vladivsc@users.noreply.github.com>
2023-01-28 16:27:54 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import styles from '../components/app.module.scss';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.app}>
     </div>
   );
 };
